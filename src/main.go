ecd7ac8d5bedbe12ab918830356a10809ed87670
Arturo Guerra <arturoguerra360@gmail.com>
2019-09-04 21:42:30 -0500

    Moved to go.mod

---
+++ src/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+  "goplex/src/config"
+  "goplex/src/handler"
+  "goplex/src/filebot"
+  "goplex/src/deluge"
+  "goplex/src/nzbget"
+  "os"
+  "fmt"
+)
+
+var configDir string
+
+
+type PlexAuto struct {
+  Config *config.Configuration
+  FileBot *filebot.FileBot
+  Deluge *deluge.Deluge
+  NzbGet *nzbget.NzbGet
+
+}
+
+func (p PlexAuto) New(configFilename string) *PlexAuto {
+  config, err := config.LoadConfig(configFilename)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(3)
+  }
+
+  FileBot := filebot.New(&config)
+  Deluge := deluge.New(&config, FileBot)
+  NzbGet := nzbget.New(&config, FileBot)
+
+  return &PlexAuto{
+    Config: &config,
+    FileBot: FileBot,
+    Deluge: Deluge,
+    NzbGet: NzbGet,
+  }
+}
+
+func main () {
+  h := handler.New()
+  args, err := h.Parse()
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(3)
+  }
+
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(3)
+  }
+
+  plex := new(PlexAuto)
+  if configDir == "" {
+      configDir = "./plexbot.conf"
+  }
+
+  plex = plex.New(configDir)
+
+  switch args.Mode {
+    case "nzbget":
+      fmt.Println("Running in nzbget mode")
+      plex.NzbGet.Handle(args.NzbGet)
+
+    case "deluge":
+      fmt.Println("Running in deluge mode")
+      plex.Deluge.Handle(args.Deluge)
+  }
+}
